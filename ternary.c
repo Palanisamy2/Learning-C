c74a180b798842577a69be23a4caff965d4fe924
Palanisamy M <146609846+Palanisamy2@users.noreply.github.com>
2024-08-15 10:18:48 +0530

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,10 @@
+int main() {
+    int a,b,c;
+    printf("Enter value of a :\n");
+    scanf("%d",&a);
+    printf("Enter value of b :\n");
+    scanf("%d",&b);
+    printf("Enter value of c :\n");
+    scanf("%d",&c);
+    a>b?a>c? printf("a is greater") : printf("c is greater"):b>c?printf("b is greater"):printf("c is greater");
+ }
